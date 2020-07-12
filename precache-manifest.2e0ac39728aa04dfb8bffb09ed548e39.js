a447342af34776f968e2b2b979d6354735e5cde5
lavish jain <lavishjain36@gmail.com>
2020-07-13 00:41:18 +0530

    Updates

---
+++ precache-manifest.2e0ac39728aa04dfb8bffb09ed548e39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abdfdb230552528bf44b6b28ca04f53",
+    "url": "/Reactproj/index.html"
+  },
+  {
+    "revision": "1dc1f44c5ef66dda74a0",
+    "url": "/Reactproj/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "48ca86a404814c21ee88",
+    "url": "/Reactproj/static/css/main.8372efec.chunk.css"
+  },
+  {
+    "revision": "1dc1f44c5ef66dda74a0",
+    "url": "/Reactproj/static/js/2.359e551c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reactproj/static/js/2.359e551c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ca86a404814c21ee88",
+    "url": "/Reactproj/static/js/main.79d69490.chunk.js"
+  },
+  {
+    "revision": "389550bb7798522b62e1",
+    "url": "/Reactproj/static/js/runtime-main.2f3bbbfb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Reactproj/static/media/SEGA.12a3db8b.woff"
+  }
+]);
